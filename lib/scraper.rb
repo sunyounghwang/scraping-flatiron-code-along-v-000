34f042e73cb8d46dc1b30b6b7300a0a3f135f286
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-25 00:28:52 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,19 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
-    def print_courses
+
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+  def get_courses
+  end
+
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,8 +23,4 @@ class Scraper
       end
     end
   end
-  
 end
-
-
-
